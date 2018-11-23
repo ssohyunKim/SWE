504c4a673952cc8f4be15a42a29e64338b647e85
yijinKim <yijin3018@gmail.com>
2018-11-23 14:26:38 +0900

    Change the type and add subMenu() in Manage.java

--- src/Manage.java
+++ src/Manage.java
@@ -1,8 +1,14 @@
 
-abstract class manage {
-	abstract void create();
+abstract class Manage {
+	abstract boolean create(Object ob);
 	abstract void view();
-	abstract void update();
-	abstract void delete();
+	abstract boolean update(Object ob);
+	abstract boolean delete(Object ob);
 	abstract void goHome();
+	
+	void subMenu(int subChoice) {
+		System.out.println("+-------------------------------------------------------------+");
+		System.out.println(" | 1) Create | 2) View | 3) Update | 4) Delete | 5) Go Home |");
+		System.out.println("+-------------------------------------------------------------+");
+	}
 }
