a29e2555a260af04c5ddc2ba429563119b0c8b9c
soosan94 <susanshin90@gmail.com>
2018-11-29 20:53:37 +0900

    Update Appointment_Manager.java

--- src/Appointment_Manager.java
+++ src/Appointment_Manager.java
@@ -1,6 +1,34 @@
 
-public class Appointment_Manager extands Asset_Manager{
+public class Appointment_Manager extends Asset_Manager{
+boolean isSuccess = true;
+	@Override
+	boolean create(Object ob) {
+		// TODO 자동 생성된 메소드 스텁
+		return isSuccess;
+	}
 
+	@Override
+	void view() {
+		// TODO 자동 생성된 메소드 스텁
+		
+	}
 
+	@Override
+	boolean update(Object ob) {
+		// TODO 자동 생성된 메소드 스텁
+		return isSuccess;
+	}
+
+	@Override
+	boolean delete(Object ob) {
+		// TODO 자동 생성된 메소드 스텁
+		return isSuccess;
+	}
+
+	@Override
+	void goHome() {
+		// TODO 자동 생성된 메소드 스텁
+		
+	}
 
 }
