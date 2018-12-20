7e29e94f4b54a84a725fd8fe061870d2cb080137
yijinKim <yijin3018@gmail.com>
2018-12-20 15:27:25 +0900

    updating

--- src/Asset_Manager.java
+++ src/Asset_Manager.java
@@ -6,7 +6,7 @@ abstract class Asset_Manager {
 	abstract boolean delete(Object ob);
 	abstract void goHome();
 	
-	void showSubMenu(int subChoice) {
+	void showSubMenu() {
 		System.out.println("+-------------------------------------------------------------+");
 		System.out.println(" | 1) Create | 2) View | 3) Update | 4) Delete | 5) Go Home |");
 		System.out.println("+-------------------------------------------------------------+");
