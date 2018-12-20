44a7939c2f3dceaadcded3a8e2759d0d4b504627
yijinKim <yijin3018@gmail.com>
2018-12-20 15:33:18 +0900

    updating

--- src/Home.java
+++ src/Home.java
@@ -1,52 +1,48 @@
-
 import java.util.Scanner;
 
 public class Home {
 
 	private static int homeChoice;
-	
-	static Scanner scanner = new Scanner(System.in);
-	//private static Contact contact;
-	//private static Manage_TodoList todolist;
-	//private static Appointment appointment;
+	private static Scanner scanner = new Scanner(System.in);
+	/*
+	 * static final int CONTACT = 1; static final int TODOLIST = 2; static final int
+	 * APPOINTMENT = 3; static final int QUIT = 4;
+	 */
+	//static Contact_Manager contactManager;
+	static TodoList_Manager todolistManager;
+	//static Appointment_Manager apointmentManager;
 	
 	public static void main(String[] args) {
 		selectHomeMenu();
 	}
-	
-	static void selectHomeMenu() {
-		
-		boolean flag = true;
-		//todolist = new Manage_TodoList();
-		
-		while(flag) {
-			System.out.println("+---------------------+");
-			System.out.println(" |  1. Contact              |");
-			System.out.println(" |  2. To-do List          |");
-			System.out.println(" |  3. Appointment     |");
-			System.out.println(" |  4. Quit                   |");
-			System.out.println("+---------------------+");
 
+	private static void selectHomeMenu() {
+		boolean isHomeMenu = true;
+
+		while (isHomeMenu) {
+			showHomeMenu();
 			homeChoice = scanner.nextInt();
 
 			if (homeChoice == 1) {
-				//contact
-				System.out.println("+------Contact------+");
+				System.out.println("+---------------------------Contact----------------------------+");
 
 			} else if (homeChoice == 2) {
-				//todolist
-				System.out.println("+-----To-do List-----+");
-				
-			
+				System.out.println("+---------------------------To-do List--------------------------+");
+				todolistManager = new TodoList_Manager();
+				todolistManager.manageTodoList();
 			} else if (homeChoice == 3) {
-				//appointment
-				System.out.println("+---Appointment---+");
-
-
+				System.out.println("+------------------------Appointment-------------------------+");
 			} else if (homeChoice == 4) {
-				System.out.println("***Thank You! Bye!***");
-				flag = false;
+				System.out.println("**********************Thank You! Bye!*************************");
+				isHomeMenu = false;
 			}
 		}
+	}
+
+	private static void showHomeMenu() {
+		System.out.println("+-------------------------Home Menu-------------------------+");
+		System.out.println("||   [1] Contact    [2] To-do List    [3] Appointment    [4] Quit  ||");
+		System.out.println("+----------------------------------------------------------------+");
+		System.out.print("Choose Home Menu: ");
 	}
 }
--- src/TodoList_Manager.java
+++ src/TodoList_Manager.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
 
 public class TodoList_Manager extends Asset_Manager{
-
+	private Scanner scanner = new Scanner(System.in);
+	private static int subMenuChoice = 0;
+	static final int CREATE = 1;
+	static final int VIEW = 2;
+	static final int UPDATE = 3;
+	static final int DELETE = 4;
+	static final int GOHOME = 5;
 	boolean isSuccess = true;
+	
+	public void manageTodoList() {
+		boolean isSubMenu = true;
+
+		while (isSubMenu) {
+			Asset_Manager assetManager = new TodoList_Manager();
+			assetManager.showSubMenu();
+			subMenuChoice = scanner.nextInt();
+
+			if (subMenuChoice == CREATE) {
+				System.out.println("CREATE");
+			} else if (subMenuChoice == VIEW) {
+				System.out.println("VIEW");
+			} else if (subMenuChoice == UPDATE) {
+				System.out.println("UPDATE");
+			} else if (subMenuChoice == DELETE) {
+				System.out.println("DELETE");
+			} else if (subMenuChoice == GOHOME) {
+				System.out.println("GOHOME");
+				break;
+			} else {
+				System.out.println("Choose valid sub menu!");
+			}
+		}
+	}
+	
 	@Override
 	boolean create(Object ob) {
 		// TODO 자동 생성된 메소드 스텁
