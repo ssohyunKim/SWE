d94dd69da13bcb3bb7a0d6ed09e66a6f388a8d92
soosan94 <susanshin90@gmail.com>
2018-12-21 13:30:03 +0900

    Update Appointment_Manager.java

--- src/Appointment_Manager.java
+++ src/Appointment_Manager.java
@@ -1,4 +1,8 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Appointment_Manager extends Asset_Manager{
@@ -21,7 +25,7 @@ public class Appointment_Manager extends Asset_Manager{
 			switch (submenuChoice) {
 			case CREATE:
 				System.out.print("Date(yy-mm-dd): ");
-				String date = scanner.nextLine();
+				String date = scanner.nextLine();	
 				System.out.print("Location: ");
 				String location = scanner.nextLine();
 				System.out.print("People: ");
@@ -35,27 +39,32 @@ public class Appointment_Manager extends Asset_Manager{
 				System.out.print("Number to Update: ");
 				int updateNumber = scanner.nextInt();
 				scanner.nextLine();
-				if(updateNumber <= 0 && updateNumber > appointments.size())
-					break;
-				appointment = appointments.get(updateNumber - 1);
-				String pre_date = ((Appointment)appointment).getDate();
-				String pre_location = ((Appointment)appointment).getLocation();
-				String pre_people = ((Appointment)appointment).getPersons();
-				
-				System.out.println("Date: " + pre_date);
-				System.out.println("Location: " + pre_location);
-				System.out.println("People: " + pre_people);
-				System.out.println();
-				System.out.print("Date(yy-mm-dd): ");
-				String new_date = scanner.nextLine();
+				String new_date = "";
+				String new_location = "";
+				String new_persons = "";
+				try {
+					appointment = appointments.get(updateNumber - 1);
+					String pre_date = ((Appointment) appointment).getDate();
+					String pre_location = ((Appointment) appointment).getLocation();
+					String pre_people = ((Appointment) appointment).getPersons();
 
-				System.out.print("Location: ");
-				String new_location = scanner.nextLine();
-				
-				System.out.print("People: ");
-				String new_persons = scanner.nextLine();
-				
-				update(appointment, new_date, new_location, new_persons);
+					System.out.println("Date: " + pre_date);
+					System.out.println("Location: " + pre_location);
+					System.out.println("People: " + pre_people);
+					System.out.println();
+					System.out.print("Date(yy-mm-dd): ");
+					new_date = scanner.nextLine();
+
+					System.out.print("Location: ");
+					new_location = scanner.nextLine();
+
+					System.out.print("People: ");
+					new_persons = scanner.nextLine();
+				} catch (Exception ex) {
+					appointment = null;
+				} finally {
+					update(appointment, new_date, new_location, new_persons);
+				}
 				break;
 			case DELETE:
 				System.out.print("Number to delete: ");
@@ -63,7 +72,7 @@ public class Appointment_Manager extends Asset_Manager{
 				scanner.nextLine();
 				if(deleteNumber <= 0 && deleteNumber > appointments.size())
 					break;
-				appointment = appointments.get(deleteNumber - 1);
+				
 				delete(appointment);
 				break;
 			case GOHOME:
@@ -81,6 +90,15 @@ public class Appointment_Manager extends Asset_Manager{
 	
 	@Override
 	boolean create(String date, String location, String persons) {
+		try {
+			DateFormat date_format = new SimpleDateFormat("yy-MM-dd");
+			date_format.setLenient(false);
+			date_format.parse(date);
+		} catch (ParseException e) {
+			System.out.println("Date Format Error!! (yy-mm-dd)");
+			return false;
+		}
+		
 		Appointment appointment = new Appointment();
 		appointment.setDate(date);	
 		appointment.setLocation(location);	
@@ -112,18 +130,37 @@ public class Appointment_Manager extends Asset_Manager{
 
 	@Override
 	boolean update(Object appointment, String new_date, String new_location, String new_persons) {
+		if(appointment == null) {
+			System.out.println("Don't exist!!");
+			return false;
+		}
+		
+		try {
+			DateFormat date_format = new SimpleDateFormat("yy-MM-dd");
+			date_format.setLenient(false);
+			date_format.parse(new_date);
+		} catch (ParseException e) {
+			System.out.println("Date Format Error!! (yy-mm-dd)");
+			return false;
+		}
+		
 		((Appointment)appointment).setDate(new_date);
 		((Appointment)appointment).setLocation(new_location);	
 		((Appointment)appointment).setPersons(new_persons);
 		System.out.println("Successfully Update!!");
-		return false;
+		return true;
 	}
 
 	@Override
-	boolean delete(Object appointment) {
+	boolean delete(int delete_no) {
+		if(delete_no <=0 || delete_no > appointments.size()) {
+			System.out.println("Don't exist!!");
+			return false;
+		}
+		Appointment appointment = appointments.get(delete_no - 1);
 		appointments.remove(appointment);
 		System.out.println("Successfully deleted!!");
-		return false;
+		return true;
 	}
 
 	@Override
@@ -134,3 +171,4 @@ public class Appointment_Manager extends Asset_Manager{
 
 
 }
+
