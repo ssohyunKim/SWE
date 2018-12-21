b35daffce869c1ca3bf60de168ad08e84bc71fbd
soosan94 <susanshin90@gmail.com>
2018-12-21 13:48:57 +0900

    Update Asset_Manager.java

--- src/Asset_Manager.java
+++ src/Asset_Manager.java
@@ -2,7 +2,7 @@ abstract class Asset_Manager {
 	abstract boolean create(String parm1, String parm2, String parm3);
 	abstract void view();
 	abstract boolean update(Object ob, String parm1, String parm2, String parm3);
-	abstract boolean delete(Object ob);
+	abstract boolean delete(int no);
 	abstract void goHome();
 	
 	void showSubMenu() {
