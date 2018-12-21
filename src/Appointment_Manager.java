b45d5d83c53bb093bd6fb94a6cc7cc6c0df6a3b3
soosan94 <susanshin90@gmail.com>
2018-12-21 12:20:31 +0900

    Update Appointment_Manager.java

--- src/Appointment_Manager.java
+++ src/Appointment_Manager.java
@@ -20,7 +20,13 @@ public class Appointment_Manager extends Asset_Manager{
 			
 			switch (submenuChoice) {
 			case CREATE:
-				create(appointment);
+				System.out.print("Date(yy-mm-dd): ");
+				String date = scanner.nextLine();
+				System.out.print("Location: ");
+				String location = scanner.nextLine();
+				System.out.print("People: ");
+				String persons = scanner.nextLine();
+				create(date,location,persons);
 				break;
 			case VIEW:
 				view();
@@ -32,7 +38,24 @@ public class Appointment_Manager extends Asset_Manager{
 				if(updateNumber <= 0 && updateNumber > appointments.size())
 					break;
 				appointment = appointments.get(updateNumber - 1);
-				update(appointment);
+				String pre_date = ((Appointment)appointment).getDate();
+				String pre_location = ((Appointment)appointment).getLocation();
+				String pre_people = ((Appointment)appointment).getPersons();
+				
+				System.out.println("Date: " + pre_date);
+				System.out.println("Location: " + pre_location);
+				System.out.println("People: " + pre_people);
+				System.out.println();
+				System.out.print("Date(yy-mm-dd): ");
+				String new_date = scanner.nextLine();
+
+				System.out.print("Location: ");
+				String new_location = scanner.nextLine();
+				
+				System.out.print("People: ");
+				String new_persons = scanner.nextLine();
+				
+				update(appointment, new_date, new_location, new_persons);
 				break;
 			case DELETE:
 				System.out.print("Number to delete: ");
@@ -57,20 +80,14 @@ public class Appointment_Manager extends Asset_Manager{
 	}
 	
 	@Override
-	boolean create(Object ob) {
+	boolean create(String date, String location, String persons) {
 		Appointment appointment = new Appointment();
-		System.out.print("Date(yy-mm-dd): ");
-		String date = scanner.nextLine();
-		appointment.setDate(date);
-		System.out.print("Location: ");
-		String location = scanner.nextLine();
-		appointment.setLocation(location);
-		System.out.print("People: ");
-		String persons = scanner.nextLine();
+		appointment.setDate(date);	
+		appointment.setLocation(location);	
 		appointment.setPersons(persons);
 		appointments.add(appointment);
 		System.out.println("Successfully Created!!");
-		return false;
+		return true;
 	}
 
 	@Override
@@ -94,24 +111,9 @@ public class Appointment_Manager extends Asset_Manager{
 	}
 
 	@Override
-	boolean update(Object appointment) {
-		String pre_date = ((Appointment)appointment).getDate();
-		String pre_location = ((Appointment)appointment).getLocation();
-		String pre_people = ((Appointment)appointment).getPersons();
-		
-		System.out.println("Date: " + pre_date);
-		System.out.println("Location: " + pre_location);
-		System.out.println("People: " + pre_people);
-		System.out.println();
-		
-		System.out.print("Date(yy-mm-dd): ");
-		String new_date = scanner.nextLine();
+	boolean update(Object appointment, String new_date, String new_location, String new_persons) {
 		((Appointment)appointment).setDate(new_date);
-		System.out.print("Location: ");
-		String new_location = scanner.nextLine();
-		((Appointment)appointment).setLocation(new_location);
-		System.out.print("People: ");
-		String new_persons = scanner.nextLine();
+		((Appointment)appointment).setLocation(new_location);	
 		((Appointment)appointment).setPersons(new_persons);
 		System.out.println("Successfully Update!!");
 		return false;
